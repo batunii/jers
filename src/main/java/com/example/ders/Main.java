ad202a88e30cdbf655826cbd2c4c0c26964ed819
batunii <sy.soni@live.com.sg>
2023-08-12 00:23:33 +0530

    Added slf4-nop.jar via Maven to remove slf4 warnings
    Signed-off-by: batunii <sy.soni@live.com.sg>

--- src/main/java/com/example/ders/Main.java
+++ src/main/java/com/example/ders/Main.java
@@ -1,9 +1,13 @@
 package com.example.ders;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileFilter;
 
 public class Main {
+    //private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
 //         Jesi jesi = new Jesi();
@@ -26,7 +30,10 @@ public class Main {
 
         //System.out.println((new Jesi()).index("C:\\Users\\Shrey\\Downloads").orElseThrow());
 
+
+
         Jesi jesi = new Jesi();
+        //logger.info("Called JESI");
         jesi.index("C:\\Users\\Shrey\\Desktop\\TestPDFs");
         System.out.println(jesi.search("Wells Fargo for Shreyansh"));
 
